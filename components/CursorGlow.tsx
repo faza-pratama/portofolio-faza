58233624cf437e1f43d93cb86539523d34bcb53c
faza-pratama <alinnopratama@gmail.com>
2026-06-25 17:19:59 +0000

    cursorglow

--- components/CursorGlow.tsx
+++ components/CursorGlow.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import About from "@/components/About";
 import { useEffect, useRef } from "react";
 
 export default function CursorGlow() {
@@ -25,6 +24,5 @@ export default function CursorGlow() {
       ref={glowRef}
       className="pointer-events-none fixed z-[999] w-[400px] h-[400px] rounded-full blur-[120px] opacity-30 -translate-x-1/2 -translate-y-1/2 bg-cyan-400"
     />
-
   );
 }
